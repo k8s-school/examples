5f8626638f4bdb7b574ad9d654d62b55d8b27330
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2021-04-28 07:56:39 +0200

    upd

--- .solution/ci.sh
+++ .solution/ci.sh
@@ -6,10 +6,11 @@ DIR=$(cd "$(dirname "$0")"; pwd -P)
 
 cp $DIR/../*.yaml $DIR
 
+echo "Installing MongoDB"
+$DIR/MONGODB-install.sh
+$DIR/MONGODB-test.sh
+
 echo "Installing queue server"
 $DIR/QUEUE-install.sh
 $DIR/QUEUE-test.sh
 
-echo "Installing MongoDB"
-$DIR/MONGODB-install.sh
-$DIR/MONGODB-test.sh
