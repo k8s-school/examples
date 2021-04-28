2abb70531fc1b18178a115c4a2cede582cc1ff2e
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2021-04-28 07:56:21 +0200

    upd

--- .solution/QUEUE-install.sh
+++ .solution/QUEUE-install.sh
@@ -9,3 +9,4 @@ kubectl  wait --for=condition=Ready pods -l app=work-queue,component=queue --all
 QUEUE_POD=$(kubectl get pods -l app=work-queue,component=queue \
 	    -o jsonpath='{.items[0].metadata.name}')
 kubectl port-forward $QUEUE_POD 8081:8080 &
+sleep 3
