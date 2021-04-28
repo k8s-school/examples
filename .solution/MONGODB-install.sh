c2ef8d7ef0ea54a0094005102d281b128aa6ecd5
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2021-04-28 07:47:07 +0200

    upd

--- .solution/MONGODB-install.sh
+++ .solution/MONGODB-install.sh
@@ -1,8 +1,9 @@
-#/bin/sh
+#/bin/bash
 
-set -e
-set -x
+set -euxo pipefail
 
 kubectl apply -f 15-12-mongo-configmap.yaml
 kubectl apply -f 15-11-mongo-service.yaml 
 kubectl apply -f 15-14-mongo-pvc.yaml 
+
+kubectl wait --timeout=-1s --for=condition=ready pod/mongo-2
