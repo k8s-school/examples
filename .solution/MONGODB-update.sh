581fb7632e4bb1244c8850815752ec57a6b97b00
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2021-06-05 07:58:57 +0200

    upd

---
+++ .solution/MONGODB-update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# See https://kubernetes.io/docs/tutorials/stateful-application/basic-stateful-set/#updating-statefulsets
+
+set -euxo pipefail
+
+DIR=$(cd "$(dirname "$0")"; pwd -P)
+
