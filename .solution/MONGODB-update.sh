73557710c66fbbbb5421a10ef61579cdb3d434e7
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2021-06-08 18:49:15 +0200

    upd

--- .solution/MONGODB-update.sh
+++ .solution/MONGODB-update.sh
@@ -6,3 +6,14 @@ set -euxo pipefail
 
 DIR=$(cd "$(dirname "$0")"; pwd -P)
 
+SF="mongo"
+
+kubectl patch statefulset "$SF" -p '{"spec":{"updateStrategy":{"type":"RollingUpdate"}}}'
+
+kubectl patch statefulset "$SF" --type='json' -p='[
+    {"op": "replace", "path": "/spec/template/spec/containers/0/image", "value":"mongo:4.4"},
+    {"op": "replace", "path": "/spec/template/spec/containers/1/image", "value":"mongo:4.4"}]'
+
+kubectl run -i --rm --tty shell --image=mongo:4.2 --restart=Never -- mongo --host mongo:27017 test --eval "db.restaurants.find()"
+
+kubectl rollout status --watch --timeout=600s statefulset/"$SF"
